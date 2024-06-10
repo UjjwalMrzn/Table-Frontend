c0035c52a1e969276ee171a4e87ce7d7b686b75e
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-06-10 19:18:13 +0545

    table occupied true and false

--- AdminPanel/src/Home.jsx
+++ AdminPanel/src/Home.jsx
@@ -1,70 +1,40 @@
-import React from 'react'
-import 
-{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
- from 'react-icons/bs'
+import React from 'react';
+import { TiTick } from 'react-icons/ti';
+import { IoCloseSharp } from 'react-icons/io5';
 
-function home() {
+function Home() {
+  const tables = [
+    { id: 1, name: 'TABLE 1', price: 'Rs.8/min', occupied: true },
+    { id: 2, name: 'TABLE 2', price: '2', occupied: false },
+    { id: 3, name: 'TABLE 3', price: '3', occupied: false },
+    { id: 4, name: 'TABLE 4', price: '4', occupied: true },
+    { id: 5, name: 'TABLE 5', price: '5', occupied: true },
+    { id: 6, name: 'TABLE 6', price: '6', occupied: false },
+  ];
+
   return (
-       
     <main className='main-container'>
-    <div className='main-title'>
+      <div className='main-title'>
         <h3>DASHBOARD</h3>
-    </div>
+      </div>
 
-    <div className='main-cards'>
-        <div className='card'>
+      <div className='main-cards'>
+        {tables.map(table => (
+          <div key={table.id} className={`card ${table.occupied ? 'occupied' : 'not-occupied'}`}>
             <div className='card-inner'>
-                <h3>TABLE 1</h3>
-                <BsFillArchiveFill className='card_icon'/>
+              <h3>{table.name}</h3>
+              {table.occupied ? (
+                <IoCloseSharp className='card_icon' />
+              ) : (
+                <TiTick className='card_icon' />
+              )}
             </div>
-            <h1>1</h1>
-        </div>
-
-        <div className='card'>
-            <div className='card-inner'>
-                <h3>TABLE 2</h3>
-                <BsFillArchiveFill className='card_icon'/>
-            </div>
-            <h1>2</h1>
-        </div>
-
-        <div className='card'>
-            <div className='card-inner'>
-                <h3>TABLE 3</h3>
-                <BsFillArchiveFill className='card_icon'/>
-            </div>
-            <h1>3</h1>
-        </div>
-
-        <div className='card'>
-            <div className='card-inner'>
-                <h3>TABLE 4</h3>
-                <BsFillArchiveFill className='card_icon'/>
-            </div>
-            <h1>4</h1>
-        </div>
-
-        <div className='card'>
-            <div className='card-inner'>
-                <h3>TABLE 5</h3>
-                <BsFillArchiveFill className='card_icon'/>
-            </div>
-            <h1>5</h1>
-        </div>
-
-        <div className='card'>
-            <div className='card-inner'>
-                <h3>TABLE 6</h3>
-                <BsFillArchiveFill className='card_icon'/>
-            </div>
-            <h1>6</h1>
-        </div>
-
-    </div>
-
+            <h1>{table.price}</h1>
+          </div>
+        ))}
+      </div>
     </main>
-
-  )
+  );
 }
 
-export default home
+export default Home;
