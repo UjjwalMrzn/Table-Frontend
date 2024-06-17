5cec014e64a9eb33b5b405b24b6c718bb17ad92e
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-06-17 13:06:10 +0545

    sidebar item href

--- AdminPanel/src/Sidebar.jsx
+++ AdminPanel/src/Sidebar.jsx
@@ -19,7 +19,7 @@ function Sidebar({openSidebarToggle, OpenSidebar}) {
 
         <ul className='sidebar-list'>
             <li className='sidebar-list-item'>
-                <a href="/">
+                <a href="/" className="sidebar-link">
                     <BsGrid1X2Fill className='icon'/> Dashboard
                 </a>
             </li>
