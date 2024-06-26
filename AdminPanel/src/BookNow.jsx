1b7c22663b0cc010f8448ed06ed9e624b94bb6d0
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-06-26 18:02:27 +0545

    booknow option, removed pagnation and delete features

--- AdminPanel/src/BookNow.jsx
+++ AdminPanel/src/BookNow.jsx
@@ -1,44 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './BookNow.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faVideo } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch, useSelector ,} from 'react-redux'; // Importing useDispatch and useSelector from react-redux
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faClock as faClockSolid } from '@fortawesome/free-solid-svg-icons';
+// import { faClock as faClockRegular } from '@fortawesome/free-regular-svg-icons';
+import { FaRegClock } from "react-icons/fa6";
+import { FiTriangle } from "react-icons/fi";
+
+
 
 const Booknow = () => {
+
+  const tablelist =useSelector(state=>state.tablelist)
+  const {error,loading ,table}=tablelist
   return (
     <>
-        {/* <div className='BookNow_box'>
-        <div className='BookNow_container'> */}
-
-            <div className='section'> 
-                {/* <h5>Choose Game Type</h5> */}
-            <div className='tile'>
-            <label for='time'>
-                <input type='checkbox'/>
-                <FontAwesomeIcon icon={faTrash} />
-                <h4>Time Based</h4>
-                </label>
-               
-
-                   
-</div>
-
-<div className='tile'>
-<label for='frame'>
-                <input type='checkbox'/>
-                <FontAwesomeIcon icon={faTrash} />
-                <h4>Frame Based</h4>
-                </label>
-
-</div></div>
-           
-                    
-            
-        {/* </div>
-        </div> */}
+    <div className='checkbox-box'>
+    <div className='chechkbox-container'>
+      <h6>Choose an option.</h6>
+    <div className="section">
+        <div className="tile">
+          <Link to={`/user/${table.id}`}>
+          <input type="checkbox" id="time"/>
+          <label htmlFor="time">
+          <FaRegClock  className="fas"/>
+            {/* <FontAwesomeIcon icon={faClockRegular} className="fas" /> */}
+            <h4>Time Based</h4>
+          </label>
+          </Link>
+        </div>
         
-    
+        
+
+        <div className="tile">
+        <Link to={`/user/${table.id}`}>
+          <input type="checkbox" id="frame" />
+          <label htmlFor="frame">
+          <FiTriangle  className="fas"/>
+            {/* <FontAwesomeIcon icon={faClockRegular} className="fas" /> */}
+            <h4>Frame Based</h4>
+          </label>
+          </Link>
+        </div>
+      </div>
+
+
+    </div>
+    </div>
+      
     </>
-  )
+  );
 }
 
-export default Booknow
+export default Booknow;
--- AdminPanel/src/Dashboard.jsx
+++ AdminPanel/src/Dashboard.jsx
@@ -4,181 +4,173 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faVideo } from '@fortawesome/free-solid-svg-icons';
 import { Link, useParams } from 'wouter'; // Importing useParams from react-router-dom
 import Popup from './Popup.jsx';
-import { useDispatch, useSelector ,} from 'react-redux'; // Importing useDispatch and useSelector from react-redux
-import { ListTable ,RemoveInfo} from './actions/tableAction'
+import { useDispatch, useSelector } from 'react-redux'; // Importing useDispatch and useSelector from react-redux
+import { ListTable, RemoveInfo } from './actions/tableAction';
 import { TiTick } from 'react-icons/ti';
 import { IoCloseSharp } from 'react-icons/io5';
-import Errormsg from './components/Errormsg'
-import Spinner from './components/Spinner'
+import Errormsg from './components/Errormsg';
+import Spinner from './components/Spinner';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 
-
-
-
 const Dashboard = () => {
+  const navigate = useNavigate();
 
-  const navigate=useNavigate()
-  // const { id } = useParams(); // Accessing 'id' from URL params
+  const dispatch = useDispatch();
+  const tablelist = useSelector(state => state.tablelist);
+  const { error, loading, table } = tablelist;
 
-  const dispatch = useDispatch()
-  const tablelist =useSelector(state=>state.tablelist)
-  const {error,loading ,table}=tablelist
-  
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(ListTable());
     dispatch(RemoveInfo());
-    
-      },[dispatch])
+  }, [dispatch]);
 
   const [isPopupVisible, setIsPopupVisible] = useState(false);
-  const [tableToDelete, setTableToDelete] = useState(null);
-  const [currentPage, setCurrentPage] = useState(0);
+  // const [tableToDelete, setTableToDelete] = useState(null);
 
-  const tablesPerPage = 6;
-  const totalPages = Math.ceil(table.length / tablesPerPage);
+  // const handleDeleteClick = (id) => {
+  //   setTableToDelete(id);
+  //   setIsPopupVisible(true);
+  // };
 
- 
+  // const handleConfirmDelete = () => {
+    // Implement your delete logic here
+    // setIsPopupVisible(false);
+    // setTableToDelete(null);
+    // Example logic:
+    // const updatedTables = tables.filter(table => table.id !== tableToDelete);
+    // setTables(updatedTables);
+    // setCurrentPage(0); // Reset to first page in case of deletion
+  // };
 
-  const handleDeleteClick = (id) => {
-    setTableToDelete(id);
-    setIsPopupVisible(true);
+  // const handleCancelDelete = () => {
+  //   setIsPopupVisible(false);
+  //   setTableToDelete(null);
+  // };
+
+  // Pagination related variables and functions are commented out for now
+  // const [currentPage, setCurrentPage] = useState(0);
+  // const tablesPerPage = 6;
+  // const totalPages = Math.ceil(table.length / tablesPerPage);
+
+  // const handleNextPage = () => {
+  //   if (currentPage < totalPages - 1) {
+  //     setCurrentPage(currentPage + 1);
+  //   }
+  // };
+
+  // const handlePreviousPage = () => {
+  //   if (currentPage > 0) {
+  //     setCurrentPage(currentPage - 1);
+  //   }
+  // };
+
+  // const currentTables = table.slice(
+  //   currentPage * tablesPerPage,
+  //   (currentPage + 1) * tablesPerPage
+  // );
+
+  const currentTables = table; // Displaying all tables without pagination
+
+  const gotouser = () => {
+    // navigate(`/User/${table.id}`); // Ensure table.id exists or handle it
   };
 
-  const handleConfirmDelete = () => {
-    const updatedTables = tables.filter(table => table.id !== tableToDelete);
-    setTables(updatedTables);
-    setIsPopupVisible(false);
-    setTableToDelete(null);
-    setCurrentPage(0); // Reset to first page in case of deletion
-  };
-
-  const handleCancelDelete = () => {
-    setIsPopupVisible(false);
-    setTableToDelete(null);
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const currentTables = table.slice(
-    currentPage * tablesPerPage,
-    (currentPage + 1) * tablesPerPage
-  );
-
-
-const gotouser =()=>{
-  navigate(`/User/${table.id}`)
-}
-
-return (
+  return (
     <div className="container-db">
-
-      {loading ? <Spinner/>
-               :error? <Errormsg>{error}</Errormsg>  
-               :<main className='main-container'>
-              
-               {/* <h1>
-                   List of Tables
-               </h1> */}
-      
-      
-          
-            <table className="appointments-table">
-              <thead>
-                <tr>
-                  <th>Table No.</th>
-                  <th>Frame Rate</th>
-                  <th>Time Rate</th>
-                  <th>Status</th>
-                  <th>A/C</th>
-                  <th>Action</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentTables.map((table, index) => (
-                  <tr key={index}>
-                    <td>{table.table_type}</td>
-                    <td>{table.rate}</td>
-                    <td>{table.price}</td>
-                    <td>
-                     
-                        
-                        <span className={`status-dot  ${table.is_running}`} />
-                    
-                    
-                    </td>
-                        <td>
-                      <span className={`AC ${table.ac}`}>
-                        {table.ac?'Available':'Not-Available'}
-                      </span>
-                    </td>
-                    <td>
-                      <Link to={`/details/${table.id}`}>
-                        <button className="pay-btn">
-                          View Details
-                        </button>
-                      </Link>
-                      <Link to={`/user/${table.id}`}>
-                      <button 
-                        
-                        className={`book-btn ${table.is_running === true ? false : ''}`}
-                        disabled={table.is_running}
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <Errormsg>{error}</Errormsg>
+      ) : (
+        <main className='main-container'>
+          <table className="appointments-table">
+            <thead>
+              <tr>
+                <th>Table No.</th>
+                <th>Frame Rate</th>
+                <th>Time Rate</th>
+                <th>Status</th>
+                <th>A/C</th>
+                <th>Action</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentTables.map((item, index) => (
+                <tr key={index}>
+                 <td>
+                    <span className={`tableno ${item.table_type}`}>{item.table_type}</span>
+                  </td>
+                  <td>{item.rate}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <span className={`status-dot ${item.is_running}`} />
+                  </td>
+                  <td>
+                    <span className={`AC ${item.ac}`}>
+                      {item.ac ? 'Available' : 'Not Available'}
+                    </span>
+                  </td>
+                  <td>
+                    <Link to={`/details/${item.id}`}>
+                      <button className="pay-btn">View Details</button>
+                    </Link>
+                    <Link to={`/BookNow`}>
+                      <button
+                        className={`book-btn ${item.is_running === true ? false : ''}`}
+                        disabled={item.is_running}
                       >
-                        {table.is_running === true ? 'Booked' : 'Book Now'}
+                        {item.is_running === true ? 'Booked' : 'Book Now'}
                       </button>
-                      </Link>
-                    </td>
-                    <td>
-                    <button className="delete-btn" onClick={() => handleDeleteClick(table.id)}>
-                        <FontAwesomeIcon icon={faTrash} />
+                    </Link>
+                    <Link to={`/user/${item.id}`}>
+                      <button
+                        className={`book-btn ${item.is_running === false ? false : ''}`}
+                        disabled={item.is_running}
+                      >
+                        {item.is_running === false ? 'Pay Now1' : 'Pay Now'}
                       </button>
-                      <Link to={`/Video/${table.id}`}> 
-                        <button className="view-btn">
-                          <FontAwesomeIcon icon={faVideo} />
-                        </button> 
-                      </Link>
-
-                    </td>
-                   
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+                    </Link>
+                  </td>
+                  <td>
+                    <button className="delete-btn" onClick={() => handleDeleteClick(item.id)}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                    <Link to={`/Video/${item.id}`}>
+                      <button className="view-btn">
+                        <FontAwesomeIcon icon={faVideo} />
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </main>
-      }
+      )}
 
-
-        {isPopupVisible && (
-          <Popup 
-            message="Are you sure you want to delete this table?" 
-            onConfirm={handleConfirmDelete} 
-            onCancel={handleCancelDelete} 
-          />
-        )}
-
-        <div className="pagination">
-          <button onClick={handlePreviousPage} disabled={currentPage === 0}>
-            Previous
-          </button>
-          <button onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
-            Next
-          </button>
-
-        </div>
-       
+      {/* {isPopupVisible && (
+        <Popup
+          message="Are you sure you want to delete this table?"
+          onConfirm={handleConfirmDelete}
+          onCancel={handleCancelDelete}
+        />
+      )} */}
     </div>
   );
 };
 
 export default Dashboard;
+
+
+
+{/* <div className="pagination">
+<button onClick={handlePreviousPage} disabled={currentPage === 0}>
+  Previous
+</button>
+<button onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
+  Next
+</button>
+
+</div> */}
