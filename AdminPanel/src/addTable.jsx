797e937662b3740e196e1a3f03cd32d40e712271
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-07-03 15:40:47 +0545

    table from

--- AdminPanel/src/addTable.jsx
+++ AdminPanel/src/addTable.jsx
@@ -1,133 +1,119 @@
-import React, { useState,useEffect } from 'react';
-import { useDispatch,useSelector } from 'react-redux'
-import Errormsg from './components/Errormsg'
-import Spinner from './components/Spinner'
-import { TableRegister,RemoveInfo } from './actions/tableAction'
-import { useNavigate, useLocation } from 'react-router-dom'
-import { Form ,Button, Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Errormsg from './components/Errormsg';
+import Spinner from './components/Spinner';
+import { TableRegister, RemoveInfo } from './actions/tableAction';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import './App.css';
 
+function AddTable() {
+  const dispatch = useDispatch();
+  const tableRegisterstore = useSelector((state) => state.tableRegisterstore);
+  const { error, loading, Info } = tableRegisterstore;
 
-function addTable() {
+  const [tableno, setTable_type] = useState('');
+  const [rate, setRate] = useState('');
+  const [price, setPrice] = useState('');
+  const [frame_time_limit, setFrame_time_limit] = useState('');
+  const [ac, setAc] = useState(false);
+  const [is_running, setIsrunning] = useState('');
 
-  const dispatch=useDispatch()
-  const tableRegisterstore =useSelector(state=>state.tableRegisterstore)
-  const {error, loading ,Info}=tableRegisterstore
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (Info) {
+      navigate('/dashboard');
+    }
+  }, [navigate, Info]);
 
-  // const [name,setName]=useState('')
-  // const [address,setAddress]=useState('')
-  // const [phonenumber,setPhonenumber]=useState('')
-  // const [email,setEmail]=useState('')
-  const [tableno,setTable_type]=useState('')     
-  const [rate,setRate]=useState('')
-  const [price,setPrice]=useState('')
-  const [frame_time_limit,setFrame_time_limit]=useState('')
-
-
-  // const [time,setTime]=useState('')
-  const [ac,setAc]=useState(false)
-  const [is_running,setIsrunning]=useState('')
-
-  // const isValidEmail =(email)=>{
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   return emailRegex.test(email);
-  // }
-
-  const navigate = useNavigate()
-  // const [shouldValidate, setShouldValidate] = useState(false);
-
- 
-  useEffect(()=>{
-  if (Info){
-    
-        navigate('/dashboard')
-   
-    // setShouldValidate(false);
-  }
-
-},[navigate,Info])
-
-
-useEffect(()=>{
- 
+  useEffect(() => {
     setIsrunning(false);
-  
-})
-const submitHandler=(e)=>{
-  e.preventDefault();  // Prevent default form submission
-  dispatch(TableRegister(
-    tableno,
-    rate,
-    price,
-    frame_time_limit,
-    ac,
-    is_running
-  ))
-  
-  // dispatch(TableRegister(Name,Address,Phonenumber,Email,Table_type,Rate,Price,Frame,Frame_time_limit,Ac))
-  // setShouldValidate(true);
+  }, []);
 
-}
-const checked =Boolean
-
+  const submitHandler = (e) => {
+    e.preventDefault(); // Prevent default form submission
+    dispatch(
+      TableRegister(tableno, rate, price, frame_time_limit, ac, is_running)
+    );
+  };
 
   return (
-    <div className='form-box'>
-      <div className='form-container'>
+    <div className="form-box">
+      <div className="form-container">
         <h2>New Table</h2>
-        {error && <Errormsg varient='danger'>{ error }</Errormsg> }
-        {loading && <Spinner/>}
+        {error && <Errormsg variant="danger">{error}</Errormsg>}
+        {loading && <Spinner />}
         <Form onSubmit={submitHandler}>
-          <div className='form-group'>
-            <label>Table Number</label>
-            <input type='number' name='tableno' value={tableno} onChange={(e)=>setTable_type(e.target.value)}  />
-          </div>
-          <div className='form-group'>
-            <label>Time Rate</label>
-            <input type='number' name='Rate' value={rate} onChange={(e)=>setRate(e.target.value)}  />
-          </div>
-          <div className='form-group'>
-            <label>Frame Price</label>
-            <input type='number' name='Price' value={price} onChange={(e)=>setPrice(e.target.value)}  />
-          </div>
-          
-          <div className='form-group'>
-            <label>Frame Time Limit</label>
-            <input type='time' name='Frame_time_limit' value={frame_time_limit} onChange={(e)=>setFrame_time_limit(e.target.value)}  />
-          </div>
-          <div className='form-group'>
-            <label>A/C</label>
-            <input type='checkbox' name='Ac' checked={ac}  onChange={(e)=>setAc(e.target.checked)}  />
-          </div>
-          
-          {/* <div className='form-group'>
-            <label>Ac</label>
-            <input type='checkbox' name='Ac' checked={Ac}  onChange={(e)=>setAc(e.target.checked)}  />
-          </div> */}
-    
-          {/* <div class="select-box">
-            <select required>
-              
-            <option disabled selected hidden>Select an option</option>
-            <option value="Frame">Frame-Based</option>
-              <option value="Time">Time-Based</option>
-            </select>
-          </div> */}
-          <div className='submit-btn'>
-            <button type='submit'>Submit</button>
+          <div className="form-group">
+            <label>Table Number :</label>
+            <div className='input-with-unitt'>
+            <input
+              type="number"
+              name="tableno"
+              value={tableno}
+              onChange={(e) => setTable_type(e.target.value)}
+            />
             </div>
-         
+          </div>
+          <div className="form-group">
+            <label>Time Rate :</label>
+            <div className="input-with-unit">
+            {/* <span className="unit">Rs.</span> */}
+
+              <input
+                type="number"
+                name="Rate"
+                value={rate}
+                onChange={(e) => setRate(e.target.value)}
+              />
+              <span className="unit">/ min.</span>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Frame Price :</label>
+            <div className="input-with-unit">
+            <span className="unit">Rs.</span>
+
+              <input
+                type="number"
+                name="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <span className="unit">/ frame</span>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Frame Time - Limit :</label>
+            <div className='input-with-unit'> <input
+              type="time"
+              name="Frame_time_limit"
+              value={frame_time_limit}
+              onChange={(e) => setFrame_time_limit(e.target.value)}
+            /></div>
+          </div>
+          <div className="form-group">
+            <label>A/C :</label>
+              <div className='input-with-unit'>
+              <label className='switch'>
+              <input
+                type="checkbox"
+                name="Ac"
+                checked={ac}
+                onChange={(e) => setAc(e.target.checked)}
+              />
+              <span className="slider round"></span>
+            </label>
+              </div>
+          </div>
+          <div className="submit-btn">
+            <button type="submit">Add Table</button>
+          </div>
         </Form>
-        
-        
       </div>
     </div>
-      
   );
 }
 
-
-
-
-export default addTable;
+export default AddTable;
