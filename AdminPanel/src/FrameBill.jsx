7b02f11ead31a465bd3fca9cc9941fa6d7bb852a
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-07-08 17:55:42 +0545

    bill now 1

--- AdminPanel/src/FrameBill.jsx
+++ AdminPanel/src/FrameBill.jsx
@@ -8,15 +8,35 @@ const FrameBill = () => {
 
     <h1>SNOOKER Pvt. Ltd.</h1>
     <h2>Kathmandu, Nepal</h2>
-    <div className='bill-header'><br/>
+    <h3>TAX INVOICE</h3>
     
-    
-    <div> Tax Invoice</div>
-    </div>
+<div className='bill-details'>
+    <p>Bill No.: #1234</p>
+    <p>Sunday, December 21, 2004, 5:37:17 AM </p>
+    <p>Employee: Mr.Kritish</p>
+</div>
 
+   
+
+<main className='main-container'>
+<table className="appointments-table">
+  <thead>
+    <tr>
+      <th>Description</th>
+      <th>Table No.</th>
+      <th>Frame</th>
+      <th>Time(Limit)</th>
+      <th>Rate</th>
+      <th>Amount</th>
+    </tr>
+  </thead>
+  <tbody>
+</tbody>
+</table>
+</main>
+</div>
        
     
-    </div>
     
   )
 }
